473969ea34c043b33f9e754dbee8e4378ba7a520
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2025-02-05 19:13:32 +0100

    add the project to github pages

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,37 +1,37 @@
-import { Link } from 'react-router-dom'
-import { useTheme, useUpdateTheme } from '../ThemeProvider/ThemeProvider'
+import { Link } from 'react-router-dom';
+import { useTheme, useUpdateTheme } from '../ThemeProvider/ThemeProvider';
 
 export default function NavBar() {
-  const toggleTheme = useUpdateTheme()
-  const darkTheme = useTheme()
+  const toggleTheme = useUpdateTheme();
+  const darkTheme = useTheme();
   return (
     <div>
-      <nav className="nav-bar">
-        <Link className="none" to="/">
-          <li>Home</li>
+      <nav className='nav-bar'>
+        <Link className='none' to='/'>
+          <li>Homeee</li>
         </Link>
-        <Link className="none" to="Tienda">
+        <Link className='none' to='Tienda'>
           <li>Tienda</li>
         </Link>
-        <Link className="none" to="Suscripcion">
+        <Link className='none' to='Suscripcion'>
           <li>Suscripción</li>
         </Link>
-        <Link className="none" to="Cuenta">
+        <Link className='none' to='Cuenta'>
           <li>Cuenta</li>
         </Link>
 
-        <Link className="none" to="Rebajas">
+        <Link className='none' to='Rebajas'>
           <li>Rebajas</li>
         </Link>
 
-        <Link className="none" to="carrito">
+        <Link className='none' to='carrito'>
           <li>Carrito</li>
         </Link>
 
-        <button className="nav-bar-button" onClick={toggleTheme}>
+        <button className='nav-bar-button' onClick={toggleTheme}>
           {darkTheme ? ' Light' : ' Dark'}
         </button>
       </nav>
     </div>
-  )
+  );
 }
